9c61eb541f1b7bfdc948ab1b1dbc3e220cd40f2b
Leo Fabiano Alves <leo.fabiano@gmail.com>
2024-09-27 13:30:27 -0300

    Added class to inkoke any API

--- src/main/java/io/leofalves/screenmatch/ScreenmatchApplication.java
+++ src/main/java/io/leofalves/screenmatch/ScreenmatchApplication.java
@@ -3,6 +3,7 @@ package io.leofalves.screenmatch;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import services.ConsumoAPI;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -14,6 +15,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("App running...");
+		String url = "https://www.omdbapi.com/?t=gilmore+girls&season=1&apikey=c6c97426";
+
+		var consumoAPI = new ConsumoAPI();
+		var json = consumoAPI.obterDados(url);
+		System.out.println(json);
+
+		//json = consumoAPI.obterDados("https://coffee.alexflipnote.dev/random.json");
+		//System.out.println(json);
 	}
 }
